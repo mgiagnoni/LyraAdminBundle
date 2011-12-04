98094155a695da4758d57f6e6a7bd6e912478e76
mgiagnoni <gimassimo@gmail.com>
2011-12-04 02:41:37 +0100

    Changed tests bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,19 +9,8 @@
  * information are in the LICENSE file distributed with this source code.
  */
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Lyra\\AdminBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
-        return true;
-    }
-});
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
