bdc5e397cf35241cdbf2b08f143c61c64a024144
mgiagnoni <gimassimo@gmail.com>
2012-03-10 16:31:06 +0100

    Added functional tests

---
+++ Tests/Functional/AdminControllerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the LyraAdminBundle package.
+ *
+ * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ *
+ * This source file is subject to the MIT license. Full copyright and license
+ * information are in the LICENSE file distributed with this source code.
+ */
+
+namespace Lyra\AdminBundle\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $manager = $container->get('lyra_admin.product.model_manager');
+
+        // Empty list
+        $crawler = $client->request('GET', '/admin/product/list');
+        $this->assertTrue($crawler->filter('td.no-records')->count() == 1);
+
+        // Add records
+        $product = $manager->create();
+        $product->setName('test');
+        $product->setDescription('description');
+        $product->setPrice('3.576');
+        $manager->save($product);
+
+        $product = $manager->create();
+        $product->setName('test2');
+        $product->setDescription('description2');
+        $product->setPrice('1.23');
+        $manager->save($product);
+
+        $crawler = $client->reload();
+
+        // List headers
+        $attrs = $crawler->filter('table.ly-list th')->extract('class');
+        // $attrs[0] is batch select box
+        $this->assertEquals('sorted-asc col-name string', $attrs[1]);
+        $this->assertEquals('col-description text', $attrs[2]);
+        $this->assertEquals('sortable col-price float', $attrs[3]);
+
+        // Sort links
+        $this->assertTrue($crawler->filter('table.ly-list th.col-description a')->count() == 0);
+        $href = $crawler->filter('table.ly-list th.col-name a')->attr('href');
+        $this->assertRegexp('#name\/desc#', $href);
+        $href = $crawler->filter('table.ly-list th.col-price a')->attr('href');
+        $this->assertRegexp('#price\/asc#', $href);
+
+        // Check order
+        $values = $crawler->filter('table.ly-list tbody tr')->children()->extract('_text');
+        $this->assertEquals('test', trim($values[1]));
+
+        // Sort list by price
+        $link = $crawler->selectLink('Price')->link();
+        $crawler = $client->click($link);
+
+        // List headers
+        $attrs = $crawler->filter('table.ly-list th')->extract('class');
+        // Column name
+        $this->assertRegexp('#sortable#', $attrs[1]);
+        // Column price
+        $this->assertRegexp('#sorted-asc#', $attrs[3]);
+
+        // Check order
+        $values = $crawler->filter('table.ly-list tbody tr')->children()->extract('_text');
+        $this->assertEquals('test2', trim($values[1]));
+    }
+
+    protected function setup()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        $em = $container->get('doctrine')->getEntityManager();
+        $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
+        $schemaTool->dropDatabase('lyra_test');
+        $metadatas = $em->getMetadataFactory()->getAllMetadata();
+        if (!empty($metadatas)) {
+            $schemaTool->createSchema($metadatas);
+        }
+    }
+}
---
+++ Tests/Functional/App/AppKernel.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the LyraAdminBundle package.
+ *
+ * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ *
+ * This source file is subject to the MIT license. Full copyright and license
+ * information are in the LICENSE file distributed with this source code.
+ */
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Lyra\AdminBundle\LyraAdminBundle(),
+
+            // dummy bundle for tests
+            new Lyra\AdminBundle\Tests\Fixture\Bundle\Product\ProductBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config.yml');
+    }
+}
