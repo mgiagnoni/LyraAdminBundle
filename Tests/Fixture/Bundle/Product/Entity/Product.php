02186f6d9c2a9e9cafbf714ba0cb21f552ae7a2e
mgiagnoni <gimassimo@gmail.com>
2012-03-10 16:31:06 +0100

    Added functional tests

---
+++ Tests/Fixture/Bundle/Product/Entity/Product.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of the LyraAdminBundle package.
+ *
+ * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ *
+ * This source file is subject to the MIT license. Full copyright and license
+ * information are in the LICENSE file distributed with this source code.
+ */
+
+namespace Lyra\AdminBundle\Tests\Fixture\Bundle\Product\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Lyra\AdminBundle\Tests\Fixture\Bundle\Product\Entity\Product
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Product
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var float $price
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
---
+++ Tests/Fixture/Bundle/Product/ProductBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the LyraAdminBundle package.
+ *
+ * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ *
+ * This source file is subject to the MIT license. Full copyright and license
+ * information are in the LICENSE file distributed with this source code.
+ */
+
+namespace Lyra\AdminBundle\Tests\Fixture\Bundle\Product;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProductBundle extends Bundle
+{
+}
