b5498832974c54b086026a2519826e03ae9fb9b0
mgiagnoni <gimassimo@gmail.com>
2012-05-31 13:39:15 +0200

    Updated copyright headers

--- Pager/PagerInterface.php
+++ Pager/PagerInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the LyraContentBundle package.
  *
- * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ * Copyright 2011-2012 Massimo Giagnoni <gimassimo@gmail.com>
  *
  * This source file is subject to the MIT license. Full copyright and license
  * information are in the LICENSE file distributed with this source code.
