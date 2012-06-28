fa7b6b3f9b42b0862269f4e474b3accc41700f01
mgiagnoni <gimassimo@gmail.com>
2012-06-28 12:50:59 +0200

    Replaced vendors script with composer

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,8 +9,25 @@
  * information are in the LICENSE file distributed with this source code.
  */
 
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (!@include __DIR__ . '/../vendor/autoload.php') {
+    echo <<<EOF
+You must set up the project dependencies, run the following commands:
+
+    wget http://getcomposer.org/composer.phar
+    php composer.phar install
+
+EOF;
+
+    exit(1);
 }
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Lyra\\AdminBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
